9cc16dc24a9d39cad9775b9bea8479fb0395c059
Robbie Paris <robbie.paris@gmail.com>
2022-05-01 21:44:28 -0400

    next2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>starting our react native adventure</Text>
+      <Text>starting our NEW react native adventure</Text>
     </View>
   );
 }
