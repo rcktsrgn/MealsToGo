c23d3b688ac65ab23b9b2f934570603187902339
Robbie Paris <robbie.paris@gmail.com>
2022-05-03 20:35:29 -0400

    4

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>starting our NEW react native adventure</Text>
+      <Text>starting YOUR NEWest react native adventure</Text>
     </View>
   );
 }
